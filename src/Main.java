80cb73fa27d1c95d6731d67ee8fe40d52926a19a
michalbukalski <michalbukalski@gmail.com>
2026-02-23 23:50:44 +0100

    linkedList

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,13 @@
+import java.util.LinkedList;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        LinkedList<String> placesToVisit = new LinkedList<>();
+        placesToVisit.add("Sydney");
+        placesToVisit.add(0,"Melbourne");
+        placesToVisit.add(1,"Brisbane");
+        System.out.println(placesToVisit);
+    }
 }
